a71bd7e2028dc995d849d7f542fa15444184f87d
Siergiejevicz <mihalook@yandex.ru>
2023-03-23 11:09:04 +0300

    project run

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,76 @@
-import '../scss/style.scss'
+import "../scss/style.scss";
+import * as burger from "./burger.js";
+import * as feedback from "./feedback.js";
+import * as phone from "./phone.js";
+import * as swiper from "./swiperInit.js";
+import * as price from "./price.js";
+import * as expand from "./expand.js";
 
-console.log('Works!')
+const blur = document.querySelector(".blur");
+const actionFeedback = document.querySelector("#feedback");
+const actionPhone = document.querySelector("#phone");
+
+/////////
+
+burger.openButton.addEventListener("click", () => {
+  burger.open();
+});
+
+burger.closeButton.addEventListener("click", () => {
+  burger.close();
+});
+
+/////////
+
+feedback.openButton.addEventListener("click", () => {
+  feedback.open();
+});
+
+actionFeedback.addEventListener("click", () => {
+  feedback.open();
+});
+
+feedback.closeButton.addEventListener("click", () => {
+  feedback.close();
+});
+
+/////////
+
+phone.openButton.addEventListener("click", () => {
+  phone.open();
+});
+
+actionPhone.addEventListener("click", () => {
+  phone.open();
+});
+
+phone.closeButton.addEventListener("click", () => {
+  phone.close();
+});
+
+/////////
+
+blur.addEventListener("click", () => {
+  burger.close();
+  feedback.close();
+  phone.close();
+});
+
+price.create();
+
+for (let i = 0; i < expand.button.length; i++) {
+  expand.button[i].addEventListener("click", () => {
+    if (expand.button[i].dataset.expanded == "true") {
+      expand.close(i);
+    } else {
+      expand.open(i);
+    }
+  });
+}
+
+window.addEventListener("resize", () => {
+  swiper.init();
+});
+swiper.init();
+
+export { blur as body };
